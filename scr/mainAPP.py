14809e50e9ce5fd5957420e1aa08c8c6375c77de
Douglasdcs <douglasdecastrosantos@hotmail.com>
2021-05-06 03:57:16 -0300

    Versão teste. Requests ok

--- scr/mainAPP.py
+++ scr/mainAPP.py
@@ -6,9 +6,9 @@ from bs4 import BeautifulSoup
 # Primeira Etapa
 
 # Realizando a entrada do nome do filme
-film = "Missão Impossível"
+film_name = "Missão Impossível"
 # Removendo acentos e colocando as letras em minúsculo
-film = film.replace(" ", "+")
+film = film_name.replace(" ", "+")
 film = unidecode(film)
 film = film.lower()
 # Montando o link de pesquisa
@@ -27,18 +27,22 @@ r = req.content
 
 # Pegando dados do arquivo HTML com o BeatifulSoup
 data = BeautifulSoup(r, 'html.parser', from_encoding='utf_8')
-# Buscando resultados da pesquisa
-result = data.find_all("h2")
+# Pega todos os resultados da pesquisa e todas as informações sobre cada um deles
+result = data.find_all("li")
 # Os filmes estão organizados em colunas na classe = "section movies-results", divididos pelo "h2"
+# Lista que armazena os nomes de cada um dos títulos dos resultados
+list_of_names = []
+# Percorre a lista dos resultados, separando apenas o nome do filme e ignorando as demais informações
+for re in result:
+    # Se o resultado não for vazio, significa que existe algum filme que corresponde à pesquisa
+    if re.find_all("h2"):
+        # Adiciona o nome do filme à lista com os nomes mostrados após a pesquisa
+        # A função contents retira apenas o conteúdo contido no trecho HTML
+        list_of_names.append(re.find_all("h2")[0].contents[1].contents[0])
 
-# Se o resultado for vazio, imprime uma mensagem e finaliza a execução
-if not result:
-    print("Filme não encontrado")
+# Encerra a execução do programa caso nenhum filme seja encontrado com a pesquisa
+if not list_of_names:
+    print("Nenhum filme encontrado com este nome: ", film_name)
     exit(0)
-else:
-    results = []
-    # Percorre a lista dos filmes e retira o conteúdo que importa
-    for re in result:
-        results.append(re.contents[1].contents[0])
-    print(results)
+
 
